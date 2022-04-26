32b589558b590398e017769fab2aa8813eb5fb8c
Khaled-M-Nour1955 <khaledmnourelden1955@gmail.com>
2022-04-26 19:24:40 +0200

    nice left of file.ts

--- src/routes/imageResizeing.ts
+++ src/routes/imageResizeing.ts
@@ -1,15 +1,11 @@
 import fs from 'fs-extra';
 import path from "path";
 import resizeProcess from './resizeProcess'
-interface ImageQuery {
-  filename?: string;
-  width?: string;
-  height?: string;
-}
+interface ParamsImage {filename?: string;width?: string;height?: string;}
 export default class Image {
 static imagesPath: string = './images';
 static thumbsPath: string = './thumbs';
-static async ImageUrlPath(params: ImageQuery): Promise<null | string>{
+static async ImageUrlPath(params: ParamsImage): Promise<null | string>{
 const imagePath: string =params.width && params.height? path.resolve(Image.thumbsPath,`${params.filename}-${params.width}x${params.height}.jpg`)
 : path.resolve(Image.imagesPath, `${params.filename}.jpg`);
 try {
@@ -19,18 +15,6 @@ try {
     return null;
   }
 } 
-static async thumbsExistsInTheImagesDir(params: ImageQuery): Promise<Boolean| undefined> {
-  if (!params.filename || !params.width || !params.height) {
-    return false;
-  }
-  const filePath: string = path.resolve(Image.thumbsPath,`${params.filename}-${params.width}x${params.height}.jpg`);
-  try {
-    await fs.access(filePath);
-    return true;
-  } catch {
-    return false;
-  }
-}
 static async createThumbs(): Promise<void>{
     try {
       await fs.access(Image.thumbsPath);
@@ -38,16 +22,10 @@ static async createThumbs(): Promise<void>{
       fs.mkdir(Image.thumbsPath);
     }
   }
-static async createThumbsFile(params: ImageQuery) : Promise<null | string>{
+static async createThumbsFile(params: ParamsImage) : Promise<null | string>{
 const filePathFull: string = path.resolve(Image.imagesPath,`${params.filename}.jpg`);
 const filePathThumb: string = path.resolve(Image.thumbsPath,`${params.filename}-${params.width}x${params.height}.jpg`);
-
-    return await resizeProcess({
-      source: filePathFull,
-      target: filePathThumb,
-      width: parseInt(params.width!),
-      height: parseInt(params.height!)
-    });
+return await resizeProcess({source: filePathFull,target: filePathThumb,width: parseInt(params.width!),height: parseInt(params.height!)});
   }
 }
 
